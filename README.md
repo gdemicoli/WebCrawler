21d484ccc448fa6a65eb697d6b8ef30b4203ab7b
Gustavo Demicoli <s3898300@stude.rmit.edu.au>
2024-05-08 11:04:46 +1000

    ReadME changes...

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Important Scrapy Terminal Functions:
         based on how its type in css.
         e.g. response.css("h1")
         Will retrieve all the heading 1 elements
+        
+        response.get() retrieves actual HTML code
+        response.css("h1::text").get() retrieves only the text
+        NOTE: .get() will only retrieve one element
+            so use .getall() to retrieve all 
+
+        response.xpath().extract
+
+    To place results in json file:
+        In terminal: scrapy crawl happycrawler -o output.json
--- digisnake/digisnake/spiders/crawling_spider.py
+++ digisnake/digisnake/spiders/crawling_spider.py
@@ -12,4 +12,17 @@ class CrawlingBug(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(allow="catalogue/category")),
+        Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item"),
     )
+
+    def parse_item(self, response):
+        yield {
+
+            # This will get the title & the price based on their data types in css
+            "title": response.css(".product_main h1::text").get(),
+            "price": response.css(".price_color::text").get(),
+            # we use the [2] because we are going to be returned 3 parts of the data
+            # we only want the 3rd one
+            "availability": response.css(".availability::text")[2].get(),
+
+        }
